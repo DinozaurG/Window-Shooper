7f1466f319bf617f4cc19848b8ffd9df0181a8b3
DinozaurG <49711433+DinozaurG@users.noreply.github.com>
2020-06-18 20:23:35 +0700

    some draw done here

--- Window Shooper/View/CurrencyTextField.swift
+++ Window Shooper/View/CurrencyTextField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2 - size / 2), width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.9035838246, green: 0.8970522285, blue: 0.9085847735, alpha: 0.8001760563)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.1195797846, green: 0.1196073666, blue: 0.1195761189, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -23,6 +38,8 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2499174736)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p,
